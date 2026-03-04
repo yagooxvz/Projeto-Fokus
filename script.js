fee8e97fd1cccc4d5f2c74540c25527e84cf9084
Yago Souza <yahosouza090@gmail.com>
2026-03-04 16:40:37 -0300

    Comunicando o fim da atividade

--- script.js
+++ script.js
@@ -74,9 +74,14 @@ function alterarContexto (contexto) {
 
 const contagemRegressiva = () => {
     if(tempoDecorridoEmSegundos <= 0) {
-        audioTempoFinalizado.play()
-        zerar()
+        audioTempoFinalizado.play()       
         alert('Tempo finalizado!') 
+        const focoAtivo = html.getAttribute('data-contexto') == 'foco'
+        if (focoAtivo) {
+            const evento = new CustomEvent('FocoFinalizado')
+            document.dispatchEvent(evento)
+        }
+        zerar()
         return
     }
     tempoDecorridoEmSegundos -= 1
